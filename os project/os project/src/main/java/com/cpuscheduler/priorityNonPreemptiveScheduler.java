3ad5cbee7a9398503128265ae690b7a5c6a4ef7f
Fady Milad <127983594+FadyMilad22@users.noreply.github.com>
2024-04-18 18:44:55 +0200

    updating

--- os project/os project/src/main/java/com/cpuscheduler/priorityNonPreemptiveScheduler.java
+++ os project/os project/src/main/java/com/cpuscheduler/priorityNonPreemptiveScheduler.java
@@ -5,14 +5,22 @@ import java.util.*;
 public class priorityNonPreemptiveScheduler implements Schedular  {
 
 
+   // int relativeCurrentTime = 0;
+
+
     @Override
     public  Queue<Process> Schedule(ArrayList<Process> processes) {return null;}
 
     public  Queue<Process> Schedule(ArrayList<Process> processes ,int currentTime ) {
 
+
+    //    relativeCurrentTime = currentTime - relativeCurrentTime;
+
         ArrayList<Process> myProcesses = (ArrayList<Process>) processes.clone();
 
         Queue<Process> queue = new LinkedList<Process>();
+
+
         int time = 0;
       while (!myProcesses.isEmpty()) {
           ArrayList<Process> arrivedProcesses = new ArrayList<Process>();
@@ -62,24 +70,44 @@ if (arrivedProcesses.isEmpty()) {
           myProcesses.remove(leastPriorityEqualArrivalTime.get(0));
           time += leastPriorityEqualArrivalTime.get(0).getBurstTime();
 
-          leastPriorityEqualArrivalTime.get(0).setRemaining_burst_time(leastPriorityEqualArrivalTime.get(0).getBurstTime());
 
 
 
-          if(leastPriorityEqualArrivalTime.get(0).getEnd_time()>= currentTime ){
-              if(currentTime != 0 && queue.isEmpty() ){
-                  leastPriorityEqualArrivalTime.get(0).setRemaining_burst_time(leastPriorityEqualArrivalTime.get(0).getBurstTime()-(currentTime-leastPriorityEqualArrivalTime.get(0).getStart_time()));
-              }
-          queue.add(leastPriorityEqualArrivalTime.get(0));
+
+          if(leastPriorityEqualArrivalTime.get(0).getRemaining_burst_time() ==0 &&leastPriorityEqualArrivalTime.get(0).getStart_time() ==-1 ) {
+              leastPriorityEqualArrivalTime.get(0).setRemaining_burst_time(leastPriorityEqualArrivalTime.get(0).getBurstTime());
+
+          }else if (queue.isEmpty()) {
+              leastPriorityEqualArrivalTime.get(0).setRemaining_burst_time(leastPriorityEqualArrivalTime.get(0).getRemaining_burst_time()+1);
+          }else {
+              leastPriorityEqualArrivalTime.get(0).setRemaining_burst_time(leastPriorityEqualArrivalTime.get(0).getRemaining_burst_time());
           }
 
-          if(leastPriorityEqualArrivalTime.get(0).getEnd_time()==0 && currentTime!=0 ){
-              if(currentTime != 0 && queue.isEmpty() ){
-                  leastPriorityEqualArrivalTime.get(0).setRemaining_burst_time(leastPriorityEqualArrivalTime.get(0).getBurstTime()-(currentTime-leastPriorityEqualArrivalTime.get(0).getStart_time()));
-              }
+
+
+          System.out.println("----------------------------------------------------");
+          System.out.println("Before filtration ");
+          printQueue(queue);
+          System.out.println("----------------------------------------------------");
+
+
+
+
+          if(leastPriorityEqualArrivalTime.get(0).getEnd_time()>= currentTime){
               queue.add(leastPriorityEqualArrivalTime.get(0));
           }
 
+          //to enter new Live which end time would equal 0 but current time != 0
+          if(leastPriorityEqualArrivalTime.get(0).getEnd_time()==0 && currentTime!=0 ){
+              queue.add(leastPriorityEqualArrivalTime.get(0));
+          }
+
+
+
+          System.out.println("----------------------------------------------------");
+          System.out.println("After filtration ");
+          printQueue(queue);
+          System.out.println("----------------------------------------------------");
       }//While
 
 
@@ -90,8 +118,8 @@ if (arrivedProcesses.isEmpty()) {
 //    public double calculate_avg_wait_time(ArrayList<Process> processes){return 0;}
     @Override
     public double calculate_avg_wait_time(ArrayList<Process> processes) {
-        int currentTime = 0;
-        setProcessStartTimeAndEndTime(processes , currentTime);
+     //   int currentTime = 0;
+    //    setProcessStartTimeAndEndTime(processes , currentTime);
         double counter = 0.0;
         for(Process process : processes){
             counter += process.getStart_time()-process.getArrivalTime();
@@ -105,7 +133,7 @@ if (arrivedProcesses.isEmpty()) {
 
     public double calculate_avg_turn_around_time(ArrayList<Process> processes) {
         int currentTime = 0;
-        setProcessStartTimeAndEndTime(processes , currentTime);
+      //  setProcessStartTimeAndEndTime(processes , currentTime);
         double counter = 0.0;
         for(Process process : processes){
             counter += process.getEnd_time()-process.getArrivalTime();
@@ -114,16 +142,20 @@ if (arrivedProcesses.isEmpty()) {
     }
 
 
-public void setProcessStartTimeAndEndTime(ArrayList<Process> processes ,int currentTime){
+public void setProcessStartTimeAndEndTime( Queue<Process>  queue){
 
 
+    Queue<Process> clonedList = new LinkedList<>() ;
+    for (Process p : queue) {
+        clonedList.add(p);
+    }
 
-    Queue<Process> queue= this.Schedule(processes , currentTime);
+    //Queue<Process> queue= this.Schedule(processes , currentTime);
 
 
     int time = 0 ;
 
-    for (Process process : queue) {
+    for (Process process : clonedList) {
         if (process.getArrivalTime()<=time){
 
             process.setStart_time(time);
@@ -140,4 +172,31 @@ public void setProcessStartTimeAndEndTime(ArrayList<Process> processes ,int curr
     }
 
     }
+
+
+    public static void printQueue(Queue<Process> queue) {
+
+        Queue<Process> clonedList = new LinkedList<>() ;
+        for (Process p : queue) {
+            clonedList.add(p);
+
+            int queueSize = queue.size(); // Get the queue size
+            int currentIndex = 0;
+
+            while (currentIndex < queueSize) {
+                Process process = clonedList.poll(); // Remove and return the head element
+                if (process != null) {
+
+                    System.out.println("Process:");
+                    System.out.println("  - Process ID: " + process.getProcessID());
+                    System.out.println("  - Remaining Burst Time: " + process.getRemaining_burst_time());
+                    System.out.println("  - Priority: " + process.getPriority());
+                    System.out.println("Start time " +process.getStart_time()+" End time "+process.getEnd_time());
+                }
+                currentIndex++;
+            }
+        }
+    }
+
+
 }
